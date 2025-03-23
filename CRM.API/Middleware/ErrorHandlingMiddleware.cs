5137f8eedbfcdb14bb700e26c3d7057e07d1b2dc
mehmetceylanavrupasarj <mehmetceylan@avrupasarj.com>
2025-03-24 00:19:02 +0300

    Implement error handling middleware

---
+++ CRM.API/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Text.Json;
+using CRM.Domain.Common;
+
+namespace CRM.API.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(RequestDelegate next, ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Beklenmeyen bir hata oluştu");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = JsonSerializer.Serialize(Result.Failure("İç sunucu hatası: " + exception.Message));
+            
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            
+            return context.Response.WriteAsync(result);
+        }
+    }
+} 
